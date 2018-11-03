fb2c8b55845d7562c167a0714db7fe9a2ae514bc
TaeHoSon <43693092+TaeHoSon@users.noreply.github.com>
2018-11-04 01:01:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/son/myapplication/Mainpage.java
@@ -0,0 +1,13 @@
+package com.example.son.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Mainpage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+    }
+}
